3da958fa66e823b562075b4905a37e5ea281a94f
Elizabeth Creary <baebaebun101@gmail.com>
2019-01-28 21:30:46 -0500

    doc controller

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,2 +1,34 @@
 class DocsController < ApplicationController
+  def index
+  end 
+  def show
+  end
+  def new 
+    @doc = Doc.new
+  end 
+  def create
+    @doc = Doc.new(doc_params)
+    if @doc.save 
+        redirect_to @doc
+    else
+        render 'new'
+    end
+  end
+  def edit
+      
+  end
+  def update
+      
+  end
+  def destroy
+    
+  end
+
+  private
+    def find_doc
+        
+    end
+    def doc_params
+        params.require(:doc).permit(:title, :content)
+    end
 end
--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,26 +1,3 @@
 class Doc < ApplicationRecord
-  def index
-  end 
-  def show
-  end
-  def new 
-  end 
-  def create
-  end
-  def edit
-      
-  end
-  def update
-      
-  end
-  def destroy
-    
-  end
 
-  private
-    def find_doc
-        
-    end
-    def doc_params
-    end
 end
