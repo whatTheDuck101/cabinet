58654b5c11d3e9c5cb656f663a0fedbb88031816
Elizabeth Creary <baebaebun101@gmail.com>
2019-01-30 15:33:28 -0500

    associaton between user and doc

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -7,10 +7,11 @@ class DocsController < ApplicationController
 
   end
   def new 
-    @doc = Doc.new
+
+    @doc = current_user.docs.build
   end 
   def create
-    @doc = Doc.new(doc_params)
+    @doc = current_user.docs.build(doc_params)
     if @doc.save 
         redirect_to @doc
     else
@@ -30,7 +31,6 @@ class DocsController < ApplicationController
   def destroy
     @doc.destroy
     redirect_to docs_path
-    
   end
 
   private
